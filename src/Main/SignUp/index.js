2415daf75e3ad8d8076c0ad26236cc0d3c25d764
mohgawaad <mohgawaad@gmail.com>
2019-07-14 19:19:18 +0200

    Final lay out3

--- src/Main/SignUp/index.js
+++ src/Main/SignUp/index.js
@@ -20,74 +20,74 @@ class SignUp extends Component {
 
 
         return (
-            <SafeAreaView style={{flex: 1}}>
+            <SafeAreaView style={{ flex: 1 }}>
 
-            <ScrollView>
-            
-                <View style={Styles.containerStyle}>
-                    <View style={Styles.backStyle}>
+                <ScrollView>
+
+                    <View style={Styles.containerStyle}>
+                        <View style={Styles.backStyle}>
+                            <Text
+                                onPress={() => this.props.navigation.goBack()}
+                                style={{ fontSize: 30 }}>&larr;</Text>
+                        </View>
+                        <View style={Styles.signUpViewStyle}>
+                            <Text style={Styles.signUpTextStyle}>SIGN UP</Text>
+                        </View>
+                        <View style={{ justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ marginTop: 70, justifyContent: 'flex-start' }}>
+                                <Text style={Styles.LabelStyle}>NAME :</Text>
+                                <Input
+                                    Inputstyle={{ borderBottomWidth: 0.5, borderWidth: 0 }}
+                                    //style={inputContainer}
+                                    keyboardType="phone-pad"
+
+                                    placeholderTextColor='white'
+                                    placeholder='Phone'
+                                //onChangeText={this.onPhoneChange.bind(this)}
+                                //value={this.props.phone}
+                                />
+
+                            </View>
+                            <View style={{ marginTop: 15, justifyContent: 'flex-start' }}>
+                                <Text style={Styles.LabelStyle}>E_MAIL :</Text>
+                                <Input
+                                    Inputstyle={{ borderBottomWidth: 0.5, borderWidth: 0 }}
+                                    //style={inputContainer}
+                                    keyboardType="phone-pad"
+
+                                    placeholderTextColor='white'
+                                    placeholder='Phone'
+
+                                //onChangeText={this.onPhoneChange.bind(this)}
+                                //value={this.props.phone}
+                                />
+                            </View>
+
+                            <View style={{ marginTop: 15, justifyContent: 'flex-start' }}>
+                                <Text style={Styles.LabelStyle}>PASSWORD :</Text>
+                                <Input
+                                    Inputstyle={{ borderBottomWidth: 0.5, borderWidth: 0 }}
+                                    //style={inputContainer}
+                                    placeholderTextColor='white'
+                                    secureTextEntry
+                                    secure={true}
+                                    placeholder='Password'
+                                //onChangeText={this.onPasswordChange.bind(this)}
+                                //value={this.props.password}
+                                />
+                            </View>
+                        </View>
+                        <View style={{ flex: 1 }}>
+                            <Button style={Styles.signUpBtnStyle} textStyle={{ fontSize: 19, fontWeight: '900', }}>SIGN UP</Button>
+                            <TouchableOpacity onPress={() => this.logInFun()} >
+                                <Text style={Styles.alreadyHaveStyle}>ALREADY HAVE AN ACCOUNT?
                         <Text
-                            onPress={() => this.props.navigation.goBack()}
-                            style={{ fontSize: 30 }}>&larr;</Text>
-                    </View>
-                    <View style={Styles.signUpViewStyle}>
-                        <Text style={Styles.signUpTextStyle}>SIGN UP</Text>
-                    </View>
-                    <View style={{ justifyContent: 'space-between', flex: 1 }}>
-                        <View style={{ marginTop: 70, justifyContent: 'flex-start' }}>
-                            <Text style={Styles.LabelStyle}>NAME :</Text>
-                            <Input
-
-                                //style={inputContainer}
-                                keyboardType="phone-pad"
-
-                                placeholderTextColor='white'
-                                placeholder='Phone'
-                            //onChangeText={this.onPhoneChange.bind(this)}
-                            //value={this.props.phone}
-                            />
-
-                        </View>
-                        <View style={{ marginTop: 15, justifyContent: 'flex-start' }}>
-                            <Text style={Styles.LabelStyle}>E_MAIL :</Text>
-                            <Input
-
-                                //style={inputContainer}
-                                keyboardType="phone-pad"
-
-                                placeholderTextColor='white'
-                                placeholder='Phone'
-
-                            //onChangeText={this.onPhoneChange.bind(this)}
-                            //value={this.props.phone}
-                            />
-                        </View>
-
-                        <View style={{ marginTop: 15, justifyContent: 'flex-start' }}>
-                            <Text style={Styles.LabelStyle}>PASSWORD :</Text>
-                            <Input
-
-                                //style={inputContainer}
-                                placeholderTextColor='white'
-                                secureTextEntry
-                                secure={true}
-                                placeholder='Password'
-                            //onChangeText={this.onPasswordChange.bind(this)}
-                            //value={this.props.password}
-                            />
+                                        style={{ color: '#8BD391', fontSize: 19 }}> Login</Text>
+                                </Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
-                    <View style={{ flex: 1 }}>
-                        <Button style={Styles.signUpBtnStyle} textStyle={{ fontSize: 19, fontWeight: '900', }}>SIGN UP</Button>
-                       <TouchableOpacity onPress={() => this.logInFun()} >
-                        <Text style={Styles.alreadyHaveStyle}>ALREADY HAVE AN ACCOUNT?
-                        <Text 
-                                style={{ color: '#8BD391',fontSize:19 }}> Login</Text>
-                        </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
             </SafeAreaView>
 
         );
